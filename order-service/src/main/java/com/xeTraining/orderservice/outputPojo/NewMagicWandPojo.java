8edb05417bdc3f27385dd595b7163536569b0089
Mat Razi, Nur Ain Najwa <nur.ain.n.mat.razi@accenture.com>
2023-02-09 09:41:05 +0800

    Updated POJO

--- order-service/src/main/java/com/xeTraining/orderservice/outputPojo/NewMagicWandPojo.java
+++ order-service/src/main/java/com/xeTraining/orderservice/outputPojo/NewMagicWandPojo.java
@@ -1,6 +1,5 @@
 package com.xeTraining.orderservice.outputPojo;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,18 +7,13 @@ import lombok.Setter;
 @Setter
 public class NewMagicWandPojo {
 
-    @JsonProperty("wand_id")
     private Long wand_id;
 
-    @JsonProperty("magic_wand_name")
     private String magic_wand_name;
 
-    @JsonProperty("age_limit")
     private int age_limit;
 
-    @JsonProperty("magic_wand_stock")
     private int magic_wand_stock;
 
-    @JsonProperty("magic_wand_desc")
     private String magic_wand_desc;
 }
--- order-service/src/main/java/com/xeTraining/orderservice/outputPojo/NewWizardPojo.java
+++ order-service/src/main/java/com/xeTraining/orderservice/outputPojo/NewWizardPojo.java
@@ -1,6 +1,5 @@
 package com.xeTraining.orderservice.outputPojo;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,16 +7,12 @@ import lombok.Setter;
 @Setter
 public class NewWizardPojo {
 
-    @JsonProperty("wizard_id")
     private Long wizard_id;
 
-    @JsonProperty("wizard_name")
     private String wizard_name;
 
-    @JsonProperty("age")
     private int age;
 
-    @JsonProperty("active_wizard")
     private String active_wizard;
 
 
